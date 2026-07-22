3df685d8ff71ef483301790f5ae276d7e3d5284c
Alfonso Pérez <alfonso.perezsanchez@hotmail.com>
2026-07-22 12:55:43 +0200

    add missing dependencies

--- arrivagal/__init__.py
+++ arrivagal/__init__.py
@@ -1,6 +1,3 @@
 from . import transport
 
-from . import api_client
-from . import api_url
-
 __all__ = ["transport"]
--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="arriva-gal",
-    version="0.1.1",
+    name="arrivagal",
+    version="0.1.2",
     author="Alfonso Pérez Sánchez",
     author_email="alfonso.perezsanchez@hotmail.com",
     description="API wrapper for Arriva Galicia",
     packages=find_packages(),
-    keywords=['bus', 'public transport', 'galicia', 'api', "arriva"]
+    keywords=['bus', 'public transport', 'galicia', 'api', "arriva"],
+    install_requires=[
+        "requests",
+    ]
 )
