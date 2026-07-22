af149541903540f6022d07d211b7d4b143281317
Alfonso Pérez <alfonso.perezsanchez@hotmail.com>
2026-07-22 12:40:09 +0200

    Bump version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="arriva-gal",
-    version="0.1.0",
+    version="0.1.1",
     author="Alfonso Pérez Sánchez",
     author_email="alfonso.perezsanchez@hotmail.com",
     description="API wrapper for Arriva Galicia",
