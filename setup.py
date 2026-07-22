ae8102d7302b801f240fdb51139aeed66465dde4
Alfonso Pérez <alfonso.perezsanchez@hotmail.com>
2026-07-22 12:39:18 +0200

    Update package name and version in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.0.0'
-DESCRIPTION = 'Python API wrapper for the galician public transport'
-
 setup(
-    name="arrivagal",
-    version="0.1",
+    name="arriva-gal",
+    version="0.1.0",
     author="Alfonso Pérez Sánchez",
     author_email="alfonso.perezsanchez@hotmail.com",
     description="API wrapper for Arriva Galicia",
